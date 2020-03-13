133993a4879673ab372fb6594852603c002bfbfa
alpha-acrobat-9971 <qsmity@gmail.com>
2020-03-13 13:12:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function logWhisper(string){
   
  
 function sayHiToGrandma(string){
-  var string;
+  var String = string;
   if (string.toLowerCase() === string){
     console.log("I can't hear you!")
   } else if (string.toUpperCase() === string){
