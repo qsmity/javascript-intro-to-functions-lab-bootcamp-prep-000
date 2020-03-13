e989c0dad8d59b4d7912b333d666b3d7578b5ef0
alpha-acrobat-9971 <qsmity@gmail.com>
2020-03-13 13:01:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function sayHiToGrandma(string){
     console.log("I can't hear you!")
   } else if (string.toUpperCase() === string){
     console.log("YES INDEED!")
-  } else if (string.toUpperCase() !== string || string.toLowerCase()!== string){
+  } else if (string === "I love you, Grandma"){
     console.log("I love you, too.")
   }
 }
