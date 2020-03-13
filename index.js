7931ffca58f2729a6b760d275f82103df0f1bc7f
alpha-acrobat-9971 <qsmity@gmail.com>
2020-03-13 13:19:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,14 +15,13 @@ function logWhisper(string){
 }
   
  
-function sayHiToGrandma(string){
-  if (string.toLowerCase() === string){
-    console.log("I can't hear you!")
-  }
-  // } else if (string.toUpperCase() === string){
-  //   console.log("YES INDEED!")
-  // } else if(string === "I love you, Grandma"){
-  //   console.log("I love you, too")
-  // }
-}
+// function sayHiToGrandma(string){
+//   if (string.toLowerCase() === string){
+//     console.log("I can't hear you!")
+//   } else if (string.toUpperCase() === string){
+//     console.log("YES INDEED!")
+//   } else if(string === "I love you, Grandma"){
+//     console.log("I love you, too")
+//   }
+// }
 
